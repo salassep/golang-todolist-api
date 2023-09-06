654b2d0f5ef750429c99271172090cdcae45957e
Salas Sepkardianto <66544116+salassep@users.noreply.github.com>
2023-09-06 20:25:43 +0800

    Create subtodo router

---
+++ routes/subtodo.route.go
@@ -0,0 +1,21 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/salassep/golang-todolist-api/controllers"
+	"github.com/salassep/golang-todolist-api/middleware"
+)
+
+type SubTodoRouteController struct {
+	subTodoController controllers.SubTodoController
+}
+
+func NewSubTodoRouteController(subTodoController controllers.SubTodoController) SubTodoRouteController {
+	return SubTodoRouteController{subTodoController}
+}
+
+func (rc *SubTodoRouteController) SubTodoRoute(rg *gin.RouterGroup) {
+	router := rg.Group("subtodo")
+	router.Use(middleware.DeserializeUser())
+	router.POST("/", rc.subTodoController.CreateSubTodo)
+}
